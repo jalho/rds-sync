e3e69d16b8e1e54a965ad03796690b5ef292a630
jalho <alho.jere@gmail.com>
2023-12-14 22:47:31 +0200

    Send an RCON command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
     let addr: &str = "ws://127.0.0.1:8080";
     let (mut websocket, _) = tungstenite::connect(addr).unwrap();
-    let message = websocket.read().unwrap();
-    let text = message.to_text().unwrap();
+
+    let playerlist = "{\"Identifier\":1,\"Message\":\"playerlist\"}";
+    let message_out = tungstenite::protocol::Message::text(playerlist);
+    websocket.write(message_out).unwrap();
+    websocket.flush().unwrap();
+
+    let message_in = websocket.read().unwrap();
+    let text = message_in.to_text().unwrap();
     println!("{}", text);
 }
