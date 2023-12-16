2c02b19721e9a432fbb075a174b399e9cb0bb4d7
jalho <alho.jere@gmail.com>
2023-12-16 15:37:50 +0200

    Add fn send_rcon_command

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,47 @@
+use std::net::TcpStream;
+use tungstenite::{stream::MaybeTlsStream, WebSocket};
+
+#[allow(non_snake_case)]
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+struct RconResponse {
+    Message: String,
+    Identifier: u8,
+    Type: String,
+    Stacktrace: String,
+}
+
+#[allow(non_snake_case)]
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+struct RconCommand {
+    Identifier: u8,
+    Message: String,
+}
+
 fn main() {
-    let addr: &str = "ws://127.0.0.1:8080";
-    let (mut websocket, _) = tungstenite::connect(addr).unwrap();
+    let addr: &str = "ws://37.27.15.73:28016/SET_ME"; // TODO: get as input
+    let (websocket, _) = tungstenite::connect(addr).unwrap();
 
-    let playerlist = "{\"Identifier\":1,\"Message\":\"playerlist\"}";
-    let message_out = tungstenite::protocol::Message::text(playerlist);
-    websocket.write(message_out).unwrap();
-    websocket.flush().unwrap();
+    let playerlist_response = send_rcon_command(websocket, "playerlist");
+    println!("{:?}", playerlist_response);
+}
 
-    let message_in = websocket.read().unwrap();
-    let text = message_in.to_text().unwrap();
-    println!("{}", text);
+fn send_rcon_command(
+    mut socket: WebSocket<MaybeTlsStream<TcpStream>>,
+    rcon_command: &str,
+) -> String {
+    let cmd = RconCommand {
+        Identifier: 1, // TODO: construct an RCON payload with a dynamic "Identifier"
+        Message: rcon_command.to_string()
+    };
+    let cmd_serialized = serde_json::to_string(&cmd).unwrap();
+    let ws_message_out = tungstenite::protocol::Message::text(cmd_serialized);
+    socket.write(ws_message_out).unwrap();
+    socket.flush().unwrap();
+
+    // TODO: read messages till some timeout until a message with matching "Identifier is received"
+
+    let ws_message_in = socket.read().unwrap();
+    let text = ws_message_in.to_text().unwrap();
+    let p: RconResponse = serde_json::from_str(text).unwrap();
+    return p.Message;
 }
