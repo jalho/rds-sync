c7efec700d17690638a1cede9756157d695c0f94
jalho <alho.jere@gmail.com>
2024-01-11 21:57:33 +0200

    Handle WebSocket in a dedicated thread

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 use std::{
     net::{TcpListener, TcpStream},
-    thread::sleep,
+    thread::{sleep, spawn},
     time::Duration,
 };
 
 use tungstenite::{accept, WebSocket};
 
+fn handle(mut websocket: WebSocket<TcpStream>) {
+    match websocket.write("foo".into()) {
+        Ok(_) => {}
+        Err(_) => todo!(),
+    }
+
+    match websocket.flush() {
+        Ok(_) => {}
+        Err(_) => todo!(),
+    }
+
+    sleep(Duration::from_millis(10000));
+}
+
 fn main() {
     let tcp_listener: TcpListener;
     let tcp_stream: TcpStream;
-    let mut websocket: WebSocket<TcpStream>;
+    let websocket: WebSocket<TcpStream>;
 
     match TcpListener::bind("127.0.0.1:8080") {
         Ok(n) => {
@@ -32,15 +46,10 @@ fn main() {
         Err(_) => todo!(),
     }
 
-    match websocket.write("foo".into()) {
+    let h = spawn(|| handle(websocket));
+
+    match h.join() {
         Ok(_) => {}
         Err(_) => todo!(),
     }
-
-    match websocket.flush() {
-        Ok(_) => {}
-        Err(_) => todo!(),
-    }
-
-    sleep(Duration::from_millis(2000));
 }
