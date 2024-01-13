8674ddb0c3dccd4c7faca4affb8e5f881f926ff8
jalho <alho.jere@gmail.com>
2024-01-13 11:59:24 +0200

    Connect to upstream

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use std::{
     time::{Duration, Instant},
 };
 
-use tungstenite::{accept, Message, WebSocket};
+use tungstenite::{accept, connect, stream::MaybeTlsStream, Message, WebSocket};
+
+mod rcon;
 
 fn handle(websocket: WebSocket<TcpStream>) {
     let (sender, receiver) = channel::<Message>();
@@ -71,12 +73,52 @@ fn handle(websocket: WebSocket<TcpStream>) {
 fn main() {
     let tcp_listener: TcpListener;
 
+    /* Handle to RCON upstream WebSocket, intended for the remote-to-local RCON
+    state synchronizing thread. */
+    let rcon_upstream_sync: Arc<Mutex<WebSocket<MaybeTlsStream<TcpStream>>>>;
+
+    /* Handle to RCON upstream WebSocket, intended for the connected downstream
+    clients handling threads' RCON command passing. */
+    let rcon_upstream_cmd: Arc<Mutex<WebSocket<MaybeTlsStream<TcpStream>>>>;
+
+    let rcon_state_write = Arc::new(Mutex::new(rcon::State {
+        players: vec![],
+        tcs: vec![],
+        game_time: rcon::EnvTime(0.0),
+        sync_time_ms: 0,
+    }));
+    let rcon_state_read = Arc::clone(&rcon_state_write);
+
+    match connect("ws://192.168.0.104:28016/Your_Rcon_Password") {
+        Ok((ws, _)) => {
+            rcon_upstream_sync = Arc::new(Mutex::new(ws));
+            rcon_upstream_cmd = Arc::clone(&rcon_upstream_sync);
+        }
+        Err(_) => todo!(),
+    }
+
     match TcpListener::bind("0.0.0.0:8080") {
         Ok(n) => {
             tcp_listener = n;
         }
         Err(_) => todo!(),
     }
+
+    let rcon_upstream_sync_handle = spawn(move || loop {
+        match rcon_state_write.try_lock() {
+            // lock acquired here
+            Ok(state) => {
+                // TODO: sync upstream with local state using rcon_upstream_sync
+                println!("Syncing upstream RCON state!");
+            }
+            Err(_) => {}
+        } // lock released here
+
+        /* Wait some time before acquiring the lock again so other work can
+        continue using the RCON state (i.e. reading it, sending updates to
+        downstreams). */
+        sleep(Duration::from_millis(1));
+    });
 
     let main_listener_handle = spawn(move || loop {
         let tcp_stream: TcpStream;
@@ -106,6 +148,8 @@ fn main() {
         match accept(tcp_stream) {
             Ok(n) => {
                 websocket = n;
+                // TODO: use rcon_upstream_cmd to pass downstream sent commands to RCON upstream
+                // TODO: use rcon_state_read to send RCON state to downstream clients
                 spawn(|| handle(websocket));
             }
             /* Error here occurs e.g. when the handshake was only partially
@@ -120,6 +164,10 @@ fn main() {
     });
 
     match main_listener_handle.join() {
+        Ok(_) => {}
+        Err(_) => todo!(),
+    }
+    match rcon_upstream_sync_handle.join() {
         Ok(_) => {}
         Err(_) => todo!(),
     }
