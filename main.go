4226ae1eb8e974ee4c52b05ff4bf14074e3abf1f
jalho <alho.jere@gmail.com>
2024-04-19 16:16:59 +0300

    Format

--- main.go
+++ main.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-	"log"
-	"net"
-	"os"
+  "log"
+  "net"
+  "os"
 )
 
 func main() {
-	socketPath := "/tmp/asdasd.sock"
-	_ = os.Remove(socketPath)
-	unixAddr, err := net.ResolveUnixAddr("unixgram", socketPath)
-	if err != nil {
-		log.Fatal("Error resolving Unix address:", err)
-	}
+  socket_fs_path := "/tmp/rds-stats-collector.sock"
+  _ = os.Remove(socket_fs_path)
+  socket_unix_addr, err := net.ResolveUnixAddr("unixgram", socket_fs_path)
+  if err != nil {
+    log.Fatal("Error resolving Unix address:", err)
+  }
 
-	conn, err := net.ListenUnixgram("unixgram", unixAddr)
-	if err != nil {
-		log.Fatal("Error listening on Unix socket:", err)
-	}
-	defer conn.Close()
+  conn, err := net.ListenUnixgram("unixgram", socket_unix_addr)
+  if err != nil {
+    log.Fatal("Error listening on Unix socket:", err)
+  }
+  defer conn.Close()
 
-	buffer := make([]byte, 1024)
-	for {
-		n, _, err := conn.ReadFromUnix(buffer)
-		if err != nil {
-			log.Fatal("Error reading from Unix socket:", err)
-		}
-		log.Printf("Received data: %s\n", buffer[:n])
-	}
+  buffer := make([]byte, 1024)
+  for {
+    n, _, err := conn.ReadFromUnix(buffer)
+    if err != nil {
+      log.Fatal("Error reading from Unix socket:", err)
+    }
+    log.Printf("Received: '%s'\n", buffer[:n])
+  }
 }
 
